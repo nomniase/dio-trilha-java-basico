6e76434f6e6fc8cc3a6393d7571da2eb011a8488
(nomniase) <rsa_afonso@hotmail.com>
2023-10-18 20:31:04 -0300

    Desafio Sintaxe Concluído

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita ao usuário que digite o número da Agência
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        // Solicita ao usuário que digite o número da Conta
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpa o buffer do scanner
+
+        // Solicita ao usuário que digite o nome do Cliente
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        // Solicita ao usuário que digite o saldo da conta
+        System.out.print("Por favor, digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        // Mensagem final com os dados inseridos
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + numero + " e seu saldo " +
+                saldo + " já está disponível para saque.";
+
+        // Exibe a mensagem final
+        System.out.println(mensagem);
+
+        // Feche o scanner para liberar recursos
+        scanner.close();
     }
 }
+
+    
+
+
+
+
