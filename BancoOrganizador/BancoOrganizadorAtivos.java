4216197577d346b5d3b10fbfc8314cd2b22d59dd
(nomniase) <rsa_afonso@hotmail.com>
2023-10-22 14:15:22 -0300

    atualizações

---
+++ BancoOrganizador/BancoOrganizadorAtivos.java
@@ -0,0 +1,29 @@
+package BancoOrganizador;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BancoOrganizadorAtivos {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList < String > ativos = new ArrayList < > ();
+
+    //Entrada dos tipos de ativos
+    int quantidadeAtivos = scanner.nextInt();
+
+    // Entrada dos códigos dos ativos
+    for (int i = 0; i < quantidadeAtivos; i++) {
+    String codigoAtivo = scanner.nextLine();
+    ativos.add(codigoAtivo);
+    }
+
+    Collections.sort(ativos);
+
+    for (String ativo : ativos) {
+            System.out.println(ativo);
+
+    scanner.close();
+}
+}
+}
